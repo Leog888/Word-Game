ccc8ba3e96ee905e250893155906a5142da9839a
Leo <leonardodi.gutierrez@gmail.com>
2021-11-27 17:38:35 -0800

    adding questions to complete quiz questions

--- Quiz102.js
+++ Quiz102.js
@@ -9,3 +9,33 @@ var choiceEvent2 = document.getElementById("choice-container2");
 var choiceEvent3 = document.getElementById("choice-container3");
 var choiceEvent4 = document.getElementById("choice-container4");
 
+// create quiz questions here: 
+var questionArray = [
+	{
+		qestion:"What does HTML stand for?",
+
+		choice1: "Hyperlinks and Text Make-up Language",
+
+		choice2: "HyperText Markup Language",
+		
+		choice3: "HyperTension Machine Learning",
+
+		choice4: "How To Make Lunch",
+
+		answer: "HyperText Markup Language",
+	},
+
+	{
+		question: "What does CSS stand for?",
+
+		choice1: "",
+
+		choice2: "",
+
+		choice3: "",
+
+		choice4: "",
+
+		answer: "",
+	},
+]
