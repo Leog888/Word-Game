1f4cecf06445ce7deab5dc57053e74a622f94d3e
Leo <leonardodi.gutierrez@gmail.com>
2021-11-28 21:19:54 -0800

    completed form.js with showing user score and compare with other and localstorage

--- form.js
+++ form.js
@@ -1,5 +1,29 @@
 // Variables here 
-
+var scoreText = document.getElementById("scoreText");
+var submit = document.getElementById("save");
+var user = document.getElementById("username");
+var recentScore = localStorage.getItem("recentScore");
+var highscore = JSON.parse(localStorage.getItem("highscore"));
+if (localStorage.getItem("highscore") == null) {
+    highscore = []
+}
 // ScoreText here 
+scoreText.innerText = "Your Score: " + recentScore
 
 // function to save score 
+function saveScore(){
+    var score = {
+        name: user.value,
+        score: recentScore,
+    }
+    console.log(saveScore) // console.log --saveScore
+
+    highscore.push(score);
+
+    highscore.sort(function compareScore (a, b){
+        return b.score - a.score
+    })
+
+    highscore.splice(5)
+    localStorage.setItem("highscore", JSON.stringify(highscore))
+}
